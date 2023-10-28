ac8b968b3c5ad966723d1c8f8be239c6c650cdac
GAM3N1GHT <52677583+GAM3N1GHT@users.noreply.github.com>
2023-10-28 14:58:17 -0700

    Update Pac-Man.py
    Update 0.2
    Walls and player movement finished including tp across map.

--- Pac-Man.py
+++ Pac-Man.py
@@ -13,16 +13,16 @@ import random
 #Initiations
 pygame.init()
 Scolor = (3, 3, 3)
-screen = pygame.display.set_mode((570,690))
-Pcolor = (24, 150, 5)
+screen = pygame.display.set_mode((570,680))
 score = 0
 scorelen = 350
 textPos = 0
+moveTime = time.time()
 
 
 #Makes the wall positions and sizes
-walls = [pygame.Rect(0,50,570,30),pygame.Rect(0,660,570,30),pygame.Rect(0,50,30,210),pygame.Rect(540,50,30,210),pygame.Rect(0,240,120,30),pygame.Rect(450,240,120,30),pygame.Rect(0,300,120,30),pygame.Rect(450,300,120,30),pygame.Rect(450,270,30,30),pygame.Rect(90,270,30,30),pygame.Rect(450,360,120,30),pygame.Rect(0,360,120,30),pygame.Rect(90,360,30,90),pygame.Rect(450,360,30,90),pygame.Rect(0,420,120,30),pygame.Rect(450,420,120,30),pygame.Rect(0,420,30,690),pygame.Rect(540,420,30,690),pygame.Rect(60,110,60,30),pygame.Rect(150,110,90,30),pygame.Rect(270,80,30,60),pygame.Rect(330,110,90,30),pygame.Rect(450,110,60,30),pygame.Rect(60,170,60,30),pygame.Rect(150,170,30,150),pygame.Rect(210,170,150,30),pygame.Rect(450,110,60,30),pygame.Rect(270,170,30,90),pygame.Rect(390,170,30,150),pygame.Rect(450,170,60,30),pygame.Rect(150,230,90,30),pygame.Rect(330,230,90,30)]
-#                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                      |End of outer areas / Start at top left this \ will indicate a new line                                                              \                                                                                                                                                                                               \
+walls = [pygame.Rect(0,50,570,30),pygame.Rect(0,650,570,30),pygame.Rect(0,50,30,210),pygame.Rect(540,50,30,210),pygame.Rect(0,230,120,30),pygame.Rect(450,230,120,30),pygame.Rect(0,290,120,30),pygame.Rect(450,290,120,30),pygame.Rect(450,260,30,30),pygame.Rect(90,260,30,30),pygame.Rect(450,350,120,30),pygame.Rect(0,350,120,30),pygame.Rect(90,350,30,90),pygame.Rect(450,350,30,90),pygame.Rect(0,410,120,30),pygame.Rect(450,410,120,30),pygame.Rect(0,410,30,280),pygame.Rect(540,410,30,280),pygame.Rect(60,110,60,30),pygame.Rect(150,110,90,30),pygame.Rect(270,80,30,60),pygame.Rect(330,110,90,30),pygame.Rect(450,110,60,30),pygame.Rect(60,170,60,30),pygame.Rect(150,170,30,150),pygame.Rect(210,170,150,30),pygame.Rect(450,110,60,30),pygame.Rect(270,170,30,90),pygame.Rect(390,170,30,150),pygame.Rect(450,170,60,30),pygame.Rect(150,230,90,30),pygame.Rect(330,230,90,30),pygame.Rect(210,290,30,90),pygame.Rect(210,290,150,30),pygame.Rect(330,290,30,90),pygame.Rect(150,350,30,90),pygame.Rect(210,350,150,30),pygame.Rect(390,350,30,90),pygame.Rect(210,410,150,30),pygame.Rect(270,410,30,90),pygame.Rect(60,470,60,30),pygame.Rect(90,470,30,90),pygame.Rect(150,470,90,30),pygame.Rect(330,470,90,30),pygame.Rect(450,470,30,90),pygame.Rect(450,470,60,30),pygame.Rect(30,530,30,30),pygame.Rect(150,530,30,90),pygame.Rect(210,530,150,30),pygame.Rect(270,530,30,90),pygame.Rect(390,530,30,90),pygame.Rect(510,530,30,30),pygame.Rect(60,590,180,30),pygame.Rect(330,590,180,30)]
+#                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                      |End of outer areas / Start at top left this \ will indicate a new line                                                              \                                                                                                                                                                                               \                                                     \                                                                                \                                                                                 \                                                      \                                                                                                                                                               \                                                                                                                                                                 \
 
 #Makes the wall images
 def MakeWalls():
@@ -35,50 +35,70 @@ def MakeWalls():
 #Creates player and things the player can do
 class Player:
     def __init__(self):
-        self.Pos = [400,300]
+        self.Pos = [270,500]
+        self.Pcolor = (24, 150, 5)
         self.rotation = "R"
-        self.rect = pygame.Rect((90,80,30,30))
+        self.rect = pygame.Rect((270,500,30,30))
         #Track detections
-        self.leftD = pygame.Rect((399,315,1,1))
-        self.topD = pygame.Rect((415,299,1,1))
-        self.rightD = pygame.Rect((431,315,1,1))
-        self.bottomD = pygame.Rect((415,331,1,1))
+        self.leftD = pygame.Rect((269,500,1,30))
+        self.topD = pygame.Rect((270,499,30,1))
+        self.rightD = pygame.Rect((300,500,1,30))
+        self.bottomD = pygame.Rect((270,530,30,1))
         
     
     def draw(self):
-        pygame.draw.rect(screen,Pcolor,self.rect)
+        pygame.draw.rect(screen,self.Pcolor,self.rect)
     
-    def Move(self):
-        if(self.rotation == "R" and self.rightD.colliderect() == True):
-            xMove = 1
-            yMove = 0
-        elif(self.rotation == "U" and self.topD.colliderect() == True):
-            xMove = 0
-            yMove = -1
-        elif(self.rotation == "L" and self.leftD.colliderect() == True):
-            xMove = -1
-            yMove = 0
-        elif(self.rotation == "D" and self.bottomD.colliderect() == True):
-            xMove = 0
-            yMove = 1
-        self.rect.move_ip(xMove,yMove)
-        self.leftD.move_ip(xMove,yMove)
-        self.topD.move_ip(xMove,yMove)
-        self.rightD.move_ip(xMove,yMove)
-        self.bottomD.move_ip(xMove,yMove)
+    def Move(self,moveTimeP):
+        if(time.time() >= moveTimeP + .0035):
+            if(self.rotation == "R" and self.rightD.collidelist(walls) == -1):
+                self.rect.move_ip(1,0)
+                self.leftD.move_ip(1,0)
+                self.topD.move_ip(1,0)
+                self.rightD.move_ip(1,0)
+                self.bottomD.move_ip(1,0)
+            if(self.rotation == "U" and self.topD.collidelist(walls) == -1):
+                self.rect.move_ip(0,-1)
+                self.leftD.move_ip(0,-1)
+                self.topD.move_ip(0,-1)
+                self.rightD.move_ip(0,-1)
+                self.bottomD.move_ip(0,-1)
+            if(self.rotation == "L" and self.leftD.collidelist(walls) == -1):
+                self.rect.move_ip(-1,0)
+                self.leftD.move_ip(-1,0)
+                self.topD.move_ip(-1,0)
+                self.rightD.move_ip(-1,0)
+                self.bottomD.move_ip(-1,0)
+            if(self.rotation == "D" and self.bottomD.collidelist(walls) == -1):
+                self.rect.move_ip(0,1)
+                self.leftD.move_ip(0,1)
+                self.topD.move_ip(0,1)
+                self.rightD.move_ip(0,1)
+                self.bottomD.move_ip(0,1)
+            global moveTime
+            moveTime = time.time()
+        if(self.rect.x >= 570):
+            self.rect.move_ip(-599,0)
+            self.leftD.move_ip(-599,0)
+            self.topD.move_ip(-599,0)
+            self.rightD.move_ip(-599,0)
+            self.bottomD.move_ip(-599,0)
+        elif(self.rect.x <= -30):
+            self.rect.move_ip(599,0)
+            self.leftD.move_ip(599,0)
+            self.topD.move_ip(599,0)
+            self.rightD.move_ip(599,0)
+            self.bottomD.move_ip(599,0)
 
 
-screen.fill(Scolor)
+
 P = Player()
-P.draw()
-time.sleep(1)
-
 running = True
 while(running):
     #Draws the screen, player, and score
     screen.fill(Scolor)
-    P.draw()
     MakeWalls()
+    P.draw()
     scoreDisplay = pygame.font.SysFont('Comic Sans MS', 30)
     text_surface = scoreDisplay.render(str(score), False, (255, 255, 255))
     scorelen = 0
@@ -88,23 +108,18 @@ while(running):
     screen.blit(text_surface, (textPos,0))
 
 
-    """
-    Give the player 4 small hitboxes on each side of player.
-    Make bigger hit boxes that represent the areas the player can't go that only leave gaps that are exactly as big as the player hitbox.
+    key = pygame.key.get_pressed()
 
-    Detect when the player tries to change direction:
-        Is there a wall in that direction:
-            If so don't change anything
-        else:
-            change direction
+    if(key[pygame.K_d] and P.rightD.collidelist(walls) == -1):
+        P.rotation = "R"
+    if(key[pygame.K_a] and P.leftD.collidelist(walls) == -1):
+        P.rotation = "L"
+    if(key[pygame.K_w] and P.topD.collidelist(walls) == -1):
+        P.rotation = "U"
+    if(key[pygame.K_s] and P.bottomD.collidelist(walls) == -1):
+        P.rotation = "D"
     
-    In movement updater:
-        If wall in direction trying to move:
-            don't do anything
-        else:
-            depending on rotation move forward
-    """
-
+    P.Move(moveTime)
 
     #Quits the program if trying to quit
     for event in pygame.event.get():
