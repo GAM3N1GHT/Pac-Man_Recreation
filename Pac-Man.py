90d8533af0a9a0ac2d5a2b812c4dacffabaf8765
GAM3N1GHT <52677583+GAM3N1GHT@users.noreply.github.com>
2023-10-28 22:12:22 -0700

    Update Pac-Man.py
    Update 0.4
    Started adding ghosts. Too tired to have good logic so some stuff might be weird. Continue tomorrow.

--- Pac-Man.py
+++ Pac-Man.py
@@ -21,12 +21,14 @@ moveTime = time.time()
 place = True
 
 
+
 #Makes the wall positions and sizes
 walls = [pygame.Rect(0,50,570,30),pygame.Rect(0,650,570,30),pygame.Rect(0,50,30,210),pygame.Rect(540,50,30,210),pygame.Rect(0,230,120,30),pygame.Rect(450,230,120,30),pygame.Rect(0,290,120,30),pygame.Rect(450,290,120,30),pygame.Rect(450,260,30,30),pygame.Rect(90,260,30,30),pygame.Rect(450,350,120,30),pygame.Rect(0,350,120,30),pygame.Rect(90,350,30,90),pygame.Rect(450,350,30,90),pygame.Rect(0,410,120,30),pygame.Rect(450,410,120,30),pygame.Rect(0,410,30,280),pygame.Rect(540,410,30,280),pygame.Rect(60,110,60,30),pygame.Rect(150,110,90,30),pygame.Rect(270,80,30,60),pygame.Rect(330,110,90,30),pygame.Rect(450,110,60,30),pygame.Rect(60,170,60,30),pygame.Rect(150,170,30,150),pygame.Rect(210,170,150,30),pygame.Rect(450,110,60,30),pygame.Rect(270,170,30,90),pygame.Rect(390,170,30,150),pygame.Rect(450,170,60,30),pygame.Rect(150,230,90,30),pygame.Rect(330,230,90,30),pygame.Rect(210,290,30,90),pygame.Rect(210,290,150,30),pygame.Rect(330,290,30,90),pygame.Rect(150,350,30,90),pygame.Rect(210,350,150,30),pygame.Rect(390,350,30,90),pygame.Rect(210,410,150,30),pygame.Rect(270,410,30,90),pygame.Rect(60,470,60,30),pygame.Rect(90,470,30,90),pygame.Rect(150,470,90,30),pygame.Rect(330,470,90,30),pygame.Rect(450,470,30,90),pygame.Rect(450,470,60,30),pygame.Rect(30,530,30,30),pygame.Rect(150,530,30,90),pygame.Rect(210,530,150,30),pygame.Rect(270,530,30,90),pygame.Rect(390,530,30,90),pygame.Rect(510,530,30,30),pygame.Rect(60,590,180,30),pygame.Rect(330,590,180,30)]
 #                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                      |End of outer areas / Start at top left this \ will indicate a new line                                                              \                                                                                                                                                                                               \                                                     \                                                                                \                                                                                 \                                                      \                                                                                                                                                               \                                                                                                                                                                 \
-
+#Specifies the out of bounds areas so the pellets don't spawn there
 outabounds = [pygame.Rect(0,260,90,30),pygame.Rect(480,260,90,30),pygame.Rect(0,380,90,30),pygame.Rect(480,380,90,30),pygame.Rect(240,320,90,30)]
 
+#Creates the pellets
 pellets = []
 for y in range(95,640,15):
     for x in range(43,528,15):
@@ -52,14 +54,76 @@ def MakeWalls():
     for i in range(len(walls)):
         pygame.draw.rect(screen,(9, 75, 189),walls[i])
 
+#Makes the pellet images
 def MakePellets():
     for i in range(len(pellets)):
         pygame.draw.rect(screen,(235, 229, 52),pellets[i])
 
+
+
+class Ghost:
+    def __init__(self,type):
+        self.type = type
+        self.rect = pygame.Rect((240,320,30,30))
+        self.rotation = "R"
+        self.XPD = 0
+        self.YPD = 0
+        self.notTlist = []
+        if(self.type == 1):
+            self.color = (13, 194, 214)
+        elif(self.type == 2):
+            self.color = (214, 144, 13)
+        elif(self.type == 3):
+            self.color = (214, 13, 13)
+        elif(self.type == 4):
+            self.color = (219, 26, 187)
+        #Track detections
+        self.leftD = pygame.Rect((239,320,1,30))
+        self.topD = pygame.Rect((240,319,30,1))
+        self.rightD = pygame.Rect((270,320,1,30))
+        self.bottomD = pygame.Rect((240,350,30,1))
+    
+    def draw(self):
+        pygame.draw.rect(screen,self.color,self.rect)
+    
+    def MoveDecide(self):
+        self.notTlist.clear()
+        self.XPD = P.rect.x - self.rect.x
+        self.YPD = P.rect.y - self.rect.y
+        if(self.topD.collidelist() == -1):
+            self.notTlist.append("U")
+        if(self.rightD.collidelist() == -1):
+            self.notTlist.append("R")
+        if(self.bottomD.collidelist() == -1):
+            self.notTlist.append("D")
+        if(self.leftD.collidelist() == -1):
+            self.notTlist.append("L")
+        self.notTlist.remove(self.rotation)
+
+        
+        if(self.type == 4):
+            if(len(self.notTlist) == 2):
+                if(abs(self.XPD) <= abs(self.YPD)):
+                    #change direction to go to player
+                    if(self.XPD > 0 and "R" in self.notTlist):
+                        self.rotation = "R"
+                    elif(self.XPD < 0 and "L" in self.notTlist):
+                        self.rotation = "L"
+                    elif("R" not in self.notTlist):
+                        #self.notTlist.remove("R")
+                        self.rotation = self.notTlist[0]
+                    elif("L" not in self.notTlist):
+                        #self.notTlist.remove("L")
+                        self.rotation = self.notTlist[0]
+
+
+
+
+
+
 #Creates player and things the player can do
 class Player:
     def __init__(self):
-        self.Pos = [270,500]
         self.Pcolor = (24, 150, 5)
         self.rotation = "D"
         self.rect = pygame.Rect((270,500,30,30))
@@ -69,10 +133,12 @@ class Player:
         self.rightD = pygame.Rect((300,500,1,30))
         self.bottomD = pygame.Rect((270,530,30,1))
         
-    
+    #Creates the player image on screen
     def draw(self):
+        #Place holder for if I decide to make a custom image
         pygame.draw.rect(screen,self.Pcolor,self.rect)
     
+    #Moves the player and its wall detectors based on the way its facing
     def Move(self,moveTimeP):
         if(time.time() >= moveTimeP + .0035):
             if(self.rotation == "R" and self.rightD.collidelist(walls) == -1):
@@ -115,11 +181,11 @@ class Player:
             self.bottomD.move_ip(599,0)
 
 
-
+#Initializes the player and starts the main game loop.
 P = Player()
 running = True
 while(running):
-    #Draws the screen, player, and score
+    #Draws the screen, player, walls, pellets, and score
     screen.fill(Scolor)
     MakeWalls()
     MakePellets()
@@ -133,8 +199,8 @@ while(running):
     screen.blit(text_surface, (textPos,0))
 
 
+    #Changes player direction if there isn't a wall in the way.
     key = pygame.key.get_pressed()
-
     if(key[pygame.K_d] and P.rightD.collidelist(walls) == -1):
         P.rotation = "R"
     if(key[pygame.K_a] and P.leftD.collidelist(walls) == -1):
@@ -144,6 +210,7 @@ while(running):
     if(key[pygame.K_s] and P.bottomD.collidelist(walls) == -1):
         P.rotation = "D"
     
+    #Moves the player based on this direction, collects the pellets
     P.Move(moveTime)
     if(P.rect.collidelist(pellets) >=0):
         score += 10
