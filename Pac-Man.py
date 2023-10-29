d8370202badba79b0a569206e5f2dda5185567c5
GAM3N1GHT <52677583+GAM3N1GHT@users.noreply.github.com>
2023-10-28 20:28:14 -0700

    Update Pac-Man.py
    Update 0.3
    Pellets and score added but don't really do anything yet.

--- Pac-Man.py
+++ Pac-Man.py
@@ -14,30 +14,54 @@ import random
 pygame.init()
 Scolor = (3, 3, 3)
 screen = pygame.display.set_mode((570,680))
-score = 0
+score = -30
 scorelen = 350
 textPos = 0
 moveTime = time.time()
+place = True
 
 
 #Makes the wall positions and sizes
 walls = [pygame.Rect(0,50,570,30),pygame.Rect(0,650,570,30),pygame.Rect(0,50,30,210),pygame.Rect(540,50,30,210),pygame.Rect(0,230,120,30),pygame.Rect(450,230,120,30),pygame.Rect(0,290,120,30),pygame.Rect(450,290,120,30),pygame.Rect(450,260,30,30),pygame.Rect(90,260,30,30),pygame.Rect(450,350,120,30),pygame.Rect(0,350,120,30),pygame.Rect(90,350,30,90),pygame.Rect(450,350,30,90),pygame.Rect(0,410,120,30),pygame.Rect(450,410,120,30),pygame.Rect(0,410,30,280),pygame.Rect(540,410,30,280),pygame.Rect(60,110,60,30),pygame.Rect(150,110,90,30),pygame.Rect(270,80,30,60),pygame.Rect(330,110,90,30),pygame.Rect(450,110,60,30),pygame.Rect(60,170,60,30),pygame.Rect(150,170,30,150),pygame.Rect(210,170,150,30),pygame.Rect(450,110,60,30),pygame.Rect(270,170,30,90),pygame.Rect(390,170,30,150),pygame.Rect(450,170,60,30),pygame.Rect(150,230,90,30),pygame.Rect(330,230,90,30),pygame.Rect(210,290,30,90),pygame.Rect(210,290,150,30),pygame.Rect(330,290,30,90),pygame.Rect(150,350,30,90),pygame.Rect(210,350,150,30),pygame.Rect(390,350,30,90),pygame.Rect(210,410,150,30),pygame.Rect(270,410,30,90),pygame.Rect(60,470,60,30),pygame.Rect(90,470,30,90),pygame.Rect(150,470,90,30),pygame.Rect(330,470,90,30),pygame.Rect(450,470,30,90),pygame.Rect(450,470,60,30),pygame.Rect(30,530,30,30),pygame.Rect(150,530,30,90),pygame.Rect(210,530,150,30),pygame.Rect(270,530,30,90),pygame.Rect(390,530,30,90),pygame.Rect(510,530,30,30),pygame.Rect(60,590,180,30),pygame.Rect(330,590,180,30)]
 #                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                      |End of outer areas / Start at top left this \ will indicate a new line                                                              \                                                                                                                                                                                               \                                                     \                                                                                \                                                                                 \                                                      \                                                                                                                                                               \                                                                                                                                                                 \
 
+outabounds = [pygame.Rect(0,260,90,30),pygame.Rect(480,260,90,30),pygame.Rect(0,380,90,30),pygame.Rect(480,380,90,30),pygame.Rect(240,320,90,30)]
+
+pellets = []
+for y in range(95,640,15):
+    for x in range(43,528,15):
+        place = True
+        for a in range(len(walls)):
+            if(walls[a].collidepoint(x,y) == True):
+                place = False
+            if(walls[a].collidepoint(x+3,y-3) == True):
+                place = False
+            if(walls[a].collidepoint(x+3,y) == True):
+                place = False
+            if(walls[a].collidepoint(x,y-3) == True):
+                place = False
+        for b in range(len(outabounds)):
+            if(outabounds[b].collidepoint(x,y) == True):
+                place = False
+        if(place == True):
+            pellets.append(pygame.Rect(x,y,4,4))
+
+
 #Makes the wall images
 def MakeWalls():
     for i in range(len(walls)):
         pygame.draw.rect(screen,(9, 75, 189),walls[i])
 
-
-
+def MakePellets():
+    for i in range(len(pellets)):
+        pygame.draw.rect(screen,(235, 229, 52),pellets[i])
 
 #Creates player and things the player can do
 class Player:
     def __init__(self):
         self.Pos = [270,500]
         self.Pcolor = (24, 150, 5)
-        self.rotation = "R"
+        self.rotation = "D"
         self.rect = pygame.Rect((270,500,30,30))
         #Track detections
         self.leftD = pygame.Rect((269,500,1,30))
@@ -98,6 +122,7 @@ while(running):
     #Draws the screen, player, and score
     screen.fill(Scolor)
     MakeWalls()
+    MakePellets()
     P.draw()
     scoreDisplay = pygame.font.SysFont('Comic Sans MS', 30)
     text_surface = scoreDisplay.render(str(score), False, (255, 255, 255))
@@ -120,6 +145,9 @@ while(running):
         P.rotation = "D"
     
     P.Move(moveTime)
+    if(P.rect.collidelist(pellets) >=0):
+        score += 10
+        pellets.pop(P.rect.collidelist(pellets))
 
     #Quits the program if trying to quit
     for event in pygame.event.get():
